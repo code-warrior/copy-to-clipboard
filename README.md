fb6e505110fb12b77e16a5191da1dd0ea327e398
Roy A Vanegas <roy@thecodeeducators.com>
2016-01-16 19:02:23 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Copy to Clipboard Using JavaScript
+
+A simple JavaScript project to show how to copy content to the clipboard on a web 
+page.
+
+![Shows the project before the copy routine.](img/before.png)
+![Shows the project during the copy routine.](img/during.png)
+![Shows the project after having copied from the first text box to the 
+second.](img/after.png)
+
+— Roy Vanegas
---
+++ js/copy-to-clipboard.js
@@ -0,0 +1,17 @@
+/*jslint browser: true, plusplus: true */
+/*global window, document */
+
+var copyToClipboardTrigger = document.getElementById('copy-to-clipboard-trigger');
+
+copyToClipboardTrigger.addEventListener('click', function (event) {
+    var contentToCopy = document.getElementById('content-to-copy');
+
+    // Prevent the default behavior attached to <a>nchor elements.
+    event.preventDefault();
+
+    // Copy the entire contents of the input box with ID `content-to-copy`.
+    contentToCopy.select();
+
+    // Copy the content that was just selected.
+    document.execCommand('copy');
+});
