9a7e6e3fdf1d733771531b8ecab039974f1d1bcd
Roy Vanegas <roy@thecodeeducators.com>
2016-05-19 13:05:52 -0400

    Re-factor project to include error-checking

--- js/copy-to-clipboard.js
+++ js/copy-to-clipboard.js
@@ -1,19 +1,90 @@
-/*jslint browser: true */
+/*jslint browser: true, this */
 /*global window, document */
 
-var copyToClipboardTrigger = document.getElementById('copy-to-clipboard-trigger');
-
-copyToClipboardTrigger.addEventListener('click', function (event) {
+function CopyToClipboard() {
     'use strict';
 
-    var contentToCopy = document.getElementById('content-to-copy');
+    var body = document.querySelector('body'),
+        firstChildOfBody = body.firstElementChild,
+        errorMessageContainer = document.createElement('div'),
+        errorMessage = '',
+        errorMessageNode = document.createTextNode(errorMessage),
+        copyToClipboardTrigger = document.getElementById('copy-to-clipboard-trigger'),
+        contentToCopy = document.getElementById('content-to-copy'),
+        clipboardExampleContainer = document.getElementById('clipboard-example-container'),
+        errorsFound = false;
 
-    // Prevent the default behavior attached to <a>nchor elements.
-    event.preventDefault();
+    errorMessageContainer .id = 'error';
 
-    // Copy the entire contents of the input box with ID `content-to-copy`.
-    contentToCopy.select();
+    function checkCopyToClipboardTrigger() {
+        if (null === copyToClipboardTrigger) {
+            errorsFound = true;
+            clipboardExampleContainer.classList.add('hide');
+            errorMessageContainer.appendChild(errorMessageNode);
+            body.insertBefore(errorMessageContainer, firstChildOfBody);
+            errorMessageContainer.innerHTML += 'A reference to the element with the ID ' +
+                    '<code>copy-to-clipboard-trigger</code> could not be made, ' +
+                    'perhaps because the JS variable <code>copyToClipboardTrigger</code> ' +
+                    'was not initialized.';
+        } else {
+            if (undefined === copyToClipboardTrigger) {
+                errorsFound = true;
+                clipboardExampleContainer.classList.add('hide');
+                errorMessageContainer.appendChild(errorMessageNode);
+                body.insertBefore(errorMessageContainer, firstChildOfBody);
+                errorMessageContainer.innerHTML += 'A reference to the element with the ID ' +
+                        '<code>copy-to-clipboard-trigger</code> could not be made, ' +
+                        'perhaps because the ID is mis-spelled.';
+            }
+        }
+    }
 
-    // Copy the content that was just selected.
-    document.execCommand('copy');
-});
+    function checkContentToCopy() {
+        if (null === contentToCopy) {
+            errorsFound = true;
+            clipboardExampleContainer.classList.add('hide');
+            errorMessageContainer.appendChild(errorMessageNode);
+            body.insertBefore(errorMessageContainer, firstChildOfBody);
+            errorMessageContainer.innerHTML += 'A reference to the element with the ID ' +
+                    '<code>content-to-copy</code> could not be made, ' +
+                    'perhaps because the JS variable <code>contentToCopy</code> ' +
+                    'was not initialized.';
+        } else {
+            if (undefined === contentToCopy) {
+                errorsFound = true;
+                clipboardExampleContainer.classList.add('hide');
+                errorMessageContainer.appendChild(errorMessageNode);
+                body.insertBefore(errorMessageContainer, firstChildOfBody);
+                errorMessageContainer.innerHTML += 'A reference to the element with the ID ' +
+                        '<code>content-to-copy</code> could not be made, ' +
+                        'perhaps because the ID is mis-spelled.';
+            }
+        }
+    }
+
+    this.run = function () {
+        checkCopyToClipboardTrigger();
+
+        if (!errorsFound) {
+            checkContentToCopy();
+        }
+
+        if (!errorsFound) {
+            copyToClipboardTrigger.addEventListener('click', function (event) {
+
+                // Prevent the default behavior attached to the <a>nchor element.
+                event.preventDefault();
+
+                // Copy the entire contents of the input box whose ID is `content-to-copy`.
+                contentToCopy.select();
+
+                // Copy the content that was just selected.
+                document.execCommand('copy');
+            });
+        }
+    };
+}
+
+var copyToClipboard = new CopyToClipboard();
+
+copyToClipboard.run();
